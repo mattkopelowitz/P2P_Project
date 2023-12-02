fe7183e585f5e18444cb78f6bd28e4331db779d8
Matthew Kopelowitz <mattkopelowitz@gmail.com>
2023-12-02 16:48:12 -0500

    Fixed the LogWriter error

--- src/LogWriter.java
+++ src/LogWriter.java
@@ -15,7 +15,7 @@ public class LogWriter {
         peer = p;
         peerID = peer.peerID;
         logger = Logger.getLogger(Integer.toString(peerID));
-        String path = "./logs/peer_" + Integer.toString(peerID) + ".log";
+        String path = "logs/peer_" + Integer.toString(peerID) + ".log";
 
         try {
             fh = new FileHandler(path);
