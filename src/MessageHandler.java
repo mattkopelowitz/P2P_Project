294b9bf9c10b20f74cd24af23182a81c8694f9bf
Kendall Phillips <kstansfieldphill@ufl.edu>
2023-12-03 15:14:59 -0500

    finished switch 5

--- src/MessageHandler.java
+++ src/MessageHandler.java
@@ -216,6 +216,68 @@ public class MessageHandler implements Runnable{
 
                     break;
                 case 5: // save peer's bitfield, send interested/ not interested
+                    // setting peer bitfield
+                    BitSet recieved = BitSet.valueOf(payload);
+                    p.peerManager.get(remotePeerID).bitfield = (BitSet) recieved.clone();
+
+                    // set & send interested/ not interested
+                    if (p.bitfield.equals(recieved) || (p.bitfield.isEmpty() && recieved.isEmpty())) {
+                        try {
+                            p.send(message.notInterestedMsg(), output, remotePeerID);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else if (!recieved.isEmpty() && p.bitfield.isEmpty()) {
+                        //send interested message
+                        try {
+                            p.send(message.interestedMsg(), output, remotePeerID);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        
+                        //update interesting bits
+                        BitSet interesting = (BitSet) p.bitfield.clone();
+                        interesting.or(recieved);
+                        
+                        if(p.interestingPieces.containsKey(remotePeerID)) {
+                            if (!interesting.isEmpty()) {
+                                p.interestingPieces.replace(remotePeerID, interesting);
+                            } else { 
+                                p.interestingPieces.remove(remotePeerID);
+                            }
+                        } else {
+                            p.interestingPieces.put(remotePeerID, interesting);
+                        }
+                    } else {
+                        BitSet interesting = (BitSet) p.bitfield.clone();
+                        interesting.xor(recieved);
+
+                        //send interested or not
+                        if (!interesting.isEmpty()) {
+                            try {
+                                p.send(message.interestedMsg(), output, remotePeerID);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            try {
+                                p.send(message.notInterestedMsg(), output, remotePeerID);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        // update peer's interesting pieces map
+                        if(p.interestingPieces.containsKey(remotePeerID)) {
+                            if (!interesting.isEmpty()) {
+                                p.interestingPieces.replace(remotePeerID, interesting);
+                            } else { 
+                                p.interestingPieces.remove(remotePeerID);
+                            }
+                        } else {
+                            p.interestingPieces.put(remotePeerID, interesting);
+                        }
+                    }
 
                     break;
                 case 6: // log request, send piece if unchoked
